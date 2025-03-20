6487484d5f27f89ddeced9ea440a28c9d9be6780
Jhezmann <782265846@qq.com>
2025-03-20 19:07:56 +0800

    Add a testing output.

--- src/test/java/JenkinsTesting.java
+++ src/test/java/JenkinsTesting.java
@@ -1,8 +1,10 @@
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+@Slf4j
 public class JenkinsTesting {
     @Test
     public void jTest() {
-        System.out.println("Hello, Jenkins!");
+        log.info("Hello, Jenkins!");
     }
 }
